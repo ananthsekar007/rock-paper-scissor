2dc9a03da3b9cda25e4b0fc93c2b7af3cb747320
ananthsekar007 <ananthsekar007@gmail.com>
2023-05-24 15:48:38 +0530

    Add. functions for rps game

--- assets/js/rps.js
+++ assets/js/rps.js
@@ -1,1 +1,38 @@
 // the js code goes here
+
+let score = 0;
+
+function resetGame() {
+    score = 0;
+    document.getElementById("userChoice").textContent = "";
+    document.getElementById("compChoice").textContent = "";
+    document.getElementById("result").textContent = "";
+    document.getElementById("scoreNum").textContent = score;
+}
+
+function play(userChoice) {
+    const choices = ["rock", "paper", "scissors"];
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+
+    if (userChoice === computerChoice) {
+        displayResult(userChoice, computerChoice, "It's a tie!");
+        
+    } else if (
+        (userChoice === "rock" && computerChoice === "scissors") ||
+        (userChoice === "paper" && computerChoice === "rock") ||
+        (userChoice === "scissors" && computerChoice === "paper")
+    ) {
+        score++;
+        displayResult(userChoice, computerChoice, "You win!");
+    } else {
+        score--;
+        displayResult(userChoice, computerChoice, "Computer wins!");
+    } 
+}
+
+function displayResult(userChoice, compChoice, result) {
+    document.getElementById("userChoice").textContent = `Your choice : ${userChoice}`;
+    document.getElementById("compChoice").textContent = `Computer choice : ${compChoice}`;
+    document.getElementById("result").textContent = result;
+    document.getElementById("scoreNum").textContent = score;
+}
